56007965bc8e31310c42eeb7b3b756933bd887ae
He Li <476282190@qq.com>
2014-03-01 01:32:45 +0800

    Pending Module: signup

--- app/index.js
+++ app/index.js
@@ -56,6 +56,7 @@ $(function() {
         },function(data,text,xhqr){
             if(data.flag==2){
                 if(window.localStorage){
+                    window.localStorage.setItem("studentId",studentId);
                     window.localStorage.setItem("privateKey",data.privateKey);
                     window.location.href='app.html';
                     //$("#signin a[href='app.html']").click();
@@ -71,4 +72,64 @@ $(function() {
             stopLoad();
         });
     });
+
+    //signup
+    $("#signup a[href='#prepareUser']").click(function(event,direct){
+        if(direct){
+            return;
+        }
+        var self = this;
+
+        event.preventDefault();
+
+        //show Indicator
+        triggerLoad();
+
+        var studentId = $("#studentId").val();
+        var password = $("#signup-password").val();
+
+        getJSON("webservice/signup.action",{
+            studentId:studentId,
+            password:password
+        },function(data,text,xhqr){
+            if(data.flag==2){
+                if(window.localStorage){
+                    window.localStorage.setItem("studentId",studentId);
+                    window.localStorage.setItem("privateKey",data.privateKey);
+                    $(self).trigger('click',true);
+                    //$("#signin a[href='app.html']").click();
+                    //处理以下的几步必须过程
+                    getUserInfo();
+                }else{
+                    alert("do not support local storage! try to save private key in file");
+                }
+            }else{
+                alert("密码错误");
+            }
+        },function(jqXHR, textStatus, errorThrown){
+            alert(errorThrown);
+        },function(jqXHR, textStatus){
+            stopLoad();
+        });
+    });
+
+    function getUserInfo(){
+        getJSON("webservice/secure/getUserInfo.action",{
+            studentId:window.localStorage.getItem("studentId"),
+            privateKey:window.localStorage.getItem("privateKey")
+        },function(data,text,xhqr){
+            if(data.flag==2){
+                if(window.localStorage){
+
+                }else{
+                    alert("do not support local storage! try to save private key in file");
+                }
+            }else{
+                alert("密码错误");
+            }
+        },function(jqXHR, textStatus, errorThrown){
+            alert(errorThrown);
+        });
+    }
+
 });
